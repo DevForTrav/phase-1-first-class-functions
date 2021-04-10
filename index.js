9b05df8933bf39315d7370ee01437a7e29a7aec0
Travis Courtney <travis@angry-fly.com>
2021-04-10 16:33:31 -0400

    Completed assignment

---
+++ index.js
@@ -0,0 +1,11 @@
+function receivesAFunction(cb) {
+    return cb();
+}
+
+function returnsANamedFunction(params) {
+    return fn = () => {`This is a string ${params}`}
+}
+
+function returnsAnAnonymousFunction(params) {
+    return () => {`This is a string ${params}`}
+}
